2443ee3974ab14a434a7c5d1e436821e13b783c8
Mizakson <maxwellizakson@gmail.com>
2024-11-27 16:50:12 -0500

    Add staticAssets filepath, create styles.css

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 require("dotenv").config()
+
 const express = require("express")
 const path = require("path")
 const app = express()
+
+const indexRouter = require("./routes/indexRouter")
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }))
 
-const indexRouter = require("./routes/indexRouter")
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath))
 
 app.use("/", indexRouter)
 
