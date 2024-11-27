cb353ae3b2a3ac8b465f6392df4864e93e9c2141
Mizakson <maxwellizakson@gmail.com>
2024-11-27 11:27:27 -0500

    Test express server connection

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/", (req, res) => {
+    res.send("Hello...")
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`listening on http://localhost:${PORT}`)
+})
