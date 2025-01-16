8d217d6e5f817702294a01522ca4b46ab21c3cbe
Mizakson <maxwellizakson@gmail.com>
2025-01-16 11:27:26 -0500

    Add console.log statement for teams render

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -4,13 +4,16 @@ const db = require("../db/queries")
 async function teamsPageGet(req, res) {
     const { leagueId } = req.params
     const teams = await db.getTeamsByLeague(leagueId)
+
     
     if (teams.length > 0) {
+        console.log("Teams: ", teams)
         res.render("team", {
             teams: teams
         })
     }
     else {
+        console.log("No Teams...")
         res.render("error", {
             errorMsg: "The league you are looking for has not added any teams yet!",
         })
