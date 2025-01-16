91e315817c1fabdb76147a71e2974499d7ad80d0
Mizakson <maxwellizakson@gmail.com>
2025-01-16 11:20:32 -0500

    Add team error page

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -4,10 +4,17 @@ const db = require("../db/queries")
 async function teamsPageGet(req, res) {
     const { leagueId } = req.params
     const teams = await db.getTeamsByLeague(leagueId)
-    // get league name text here
-    res.render("team", {
-        teams: teams
-    })
+    
+    if (teams.length > 0) {
+        res.render("team", {
+            teams: teams
+        })
+    }
+    else {
+        res.render("error", {
+            errorMsg: "The league you are look for has not added a team yet!",
+        })
+    }
 }
 
 module.exports = {
