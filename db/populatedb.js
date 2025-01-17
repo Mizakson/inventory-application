4b8c3ac2e51ab6a6c58a435801faddf23de72beb
Mizakson <maxwellizakson@gmail.com>
2025-01-17 17:35:34 -0500

    Change connection string in populatedb.js

--- db/populatedb.js
+++ db/populatedb.js
@@ -40,7 +40,7 @@ VALUES ('Lautaro Martinez', 'ATT', 1), ('Manuel Neuer', 'GK', 2);
 async function main() {
     console.log("seeding...")
     const client = new Client({
-        connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@localhost:5432/inventory_app`,
+        connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`,
     })
     await client.connect()
     await client.query(SQL)
