034cfc8b7d4b04f8452deecccb149a1f72b9b0bf
Mizakson <maxwellizakson@gmail.com>
2025-01-17 17:35:17 -0500

    Implement database population script

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg")
 
 module.exports = new Pool({
     host: process.env.HOST,
-    user: process.env.USERNAME,
+    user: process.env.USER,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
     port: process.env.PORT,
--- db/populatedb.js
+++ db/populatedb.js
@@ -1,4 +1,5 @@
 const { Client } = require("pg")
+require("dotenv").config()
 
 const SQL = `
 CREATE TABLE IF NOT EXISTS leagues (
@@ -10,6 +11,10 @@ CREATE TABLE IF NOT EXISTS leagues (
 INSERT INTO leagues (league_name, country)
 VALUES ('Serie A', 'Italy'), ('Bundesliga', 'Germany');
 
+`
+
+const SQL1 = `
+
 CREATE TABLE IF NOT EXISTS teams (
     team_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     team_name VARCHAR (255),
@@ -18,7 +23,9 @@ CREATE TABLE IF NOT EXISTS teams (
 
 INSERT INTO teams (team_name, league_id)
 VALUES ('Inter Milan', 1), ('FC Bayern', 2);
+`
 
+const SQL2 = `
 CREATE TABLE IF NOT EXISTS players (
     player_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     player_name VARCHAR(255),
@@ -26,18 +33,21 @@ CREATE TABLE IF NOT EXISTS players (
     team_id INTEGER REFERENCES teams(team_id)
 );
 
-INSERT INTO teams (player_name, position, team_id)
+INSERT INTO players (player_name, position, team_id)
 VALUES ('Lautaro Martinez', 'ATT', 1), ('Manuel Neuer', 'GK', 2);
-
 `
 
 async function main() {
     console.log("seeding...")
     const client = new Client({
-        connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`
+        connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@localhost:5432/inventory_app`,
     })
     await client.connect()
     await client.query(SQL)
+    await client.query(SQL1)
+    await client.query(SQL2)
     await client.end()
     console.log("done")
 }
+
+main()
