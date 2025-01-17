78dc3008ff101ad8f86c146e7a96ffb51fb9247e
Mizakson <maxwellizakson@gmail.com>
2025-01-17 11:13:07 -0500

    Put add player button in team.ejs

--- routes/playerDetailsRouter.js
+++ routes/playerDetailsRouter.js
@@ -2,4 +2,6 @@ const { Router } = require("express")
 const playerDetailsRouter = Router()
 const playerDetailsController = require("../controllers/playerDetailsController")
 
+
+
 module.exports = playerDetailsRouter
