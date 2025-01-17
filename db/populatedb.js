6599b38238b3665fd7d727af66c964685a0bee5c
Mizakson <maxwellizakson@gmail.com>
2025-01-17 16:54:08 -0500

    Create populatedb.js

---
+++ db/populatedb.js
@@ -0,0 +1,43 @@
+const { Client } = require("pg")
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS leagues (
+    league_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    league_name VARCHAR (255),
+    country VARCHAR (255)
+);
+
+INSERT INTO leagues (league_name, country)
+VALUES ('Serie A', 'Italy'), ('Bundesliga', 'Germany');
+
+CREATE TABLE IF NOT EXISTS teams (
+    team_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    team_name VARCHAR (255),
+    league_id INTEGER REFERENCES leagues(league_id)
+);
+
+INSERT INTO teams (team_name, league_id)
+VALUES ('Inter Milan', 1), ('FC Bayern', 2);
+
+CREATE TABLE IF NOT EXISTS players (
+    player_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    player_name VARCHAR(255),
+    position player_position,
+    team_id INTEGER REFERENCES teams(team_id)
+);
+
+INSERT INTO teams (player_name, position, team_id)
+VALUES ('Lautaro Martinez', 'ATT', 1), ('Manuel Neuer', 'GK', 2);
+
+`
+
+async function main() {
+    console.log("seeding...")
+    const client = new Client({
+        connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`
+    })
+    await client.connect()
+    await client.query(SQL)
+    await client.end()
+    console.log("done")
+}
